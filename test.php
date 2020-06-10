b6a22351ccf5a5846b365932e794a698b52e9f70
tina789zhao <33618999+tina789zhao@users.noreply.github.com>
2020-06-10 12:06:50 +1000

    add icon

---
+++ test.php
@@ -0,0 +1,23 @@
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/7.15.0/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/7.15.0/firebase-analytics.js"></script>
+
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyABMkOTv0zP0-utTUmjVSr_CBZ4_S3xu1g",
+    authDomain: "goabroadstudy-1f68e.firebaseapp.com",
+    databaseURL: "https://goabroadstudy-1f68e.firebaseio.com",
+    projectId: "goabroadstudy-1f68e",
+    storageBucket: "goabroadstudy-1f68e.appspot.com",
+    messagingSenderId: "738200468714",
+    appId: "1:738200468714:web:00c09dcbada0f7e23d6b93",
+    measurementId: "G-P6FMYMV9PQ"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script> 
